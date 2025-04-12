b0d90240156f0310481e3c84d1717fc650f63355
NEWJIN <winniepooh00@naver.com>
2025-04-12 18:17:48 +0900

    feat: CTAButton 컴포넌트 구현, variation props 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import CTAButton from "@/components/buttons/CTAButton";
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <Header variation="type-back" />
       <h1 className="text-3xl font-pretendard font-bold text-primary">Trip to Travel</h1>
+      
       <p className="mt-2 text-gray-600">문체 스타일을 선택해주세요</p>
+      <div className="my-6">
+      <CTAButton label="다음 단계" variation="disabled" />
+
+      </div>
       <Footer/>
-    </div>
+    </main>
   );
 }
---
+++ src/components/buttons/CTAButton.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/utils/cn'
+
+type CTAButtonProps = {
+  variation?: 'default' | 'disabled' | 'black'
+  label?: string
+  onClick?: () => void
+}
+
+export default function CTAButton({
+  variation = 'default',
+  label = '다음 단계',
+  onClick
+}: CTAButtonProps) {
+  const isDisabled = variation === 'disabled'
+
+  return (
+    <button
+      onClick={onClick}
+      disabled={isDisabled}
+      className={cn(
+        'w-full h-14 rounded-[40px] shadow-[0px_1px_4px_0px_rgba(0,0,0,0.25)] flex items-center justify-center text-xl font-semibold font-pretendard',
+        variation === 'default' && 'bg-white text-black',
+        variation === 'disabled' && 'bg-gray-100 text-neutral-400 cursor-not-allowed',
+        variation === 'black' && 'bg-black text-white'
+      )}
+    >
+      {label}
+    </button>
+  )
+}
