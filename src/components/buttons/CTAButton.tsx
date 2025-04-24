8f746f012502f59e10f82c28f6dfb1bfe9aa9df5
NEWJIN <winniepooh00@naver.com>
2025-04-24 17:32:43 +0900

    feat: HomePage 기본 구조 구성

--- src/components/buttons/CTAButton.tsx
+++ src/components/buttons/CTAButton.tsx
@@ -18,7 +18,7 @@ export default function CTAButton({
       onClick={onClick}
       disabled={isDisabled}
       className={cn(
-        'w-full h-14 rounded-[40px] shadow-[0px_1px_4px_0px_rgba(0,0,0,0.25)] flex items-center justify-center text-xl font-semibold font-pretendard',
+        'w-[360px] h-14 rounded-[40px] p-4 shadow-[0px_1px_4px_0px_rgba(0,0,0,0.25)] flex items-center justify-center text-xl font-semibold font-pretendard',
         variation === 'default' && 'bg-white text-black',
         variation === 'disabled' && 'bg-gray-100 text-neutral-400 cursor-not-allowed',
         variation === 'black' && 'bg-black text-white'
