ec8814521baf0963ee5680b67a8061142454bb60
NEWJIN <winniepooh00@naver.com>
2025-04-26 08:58:13 +0900

    feat: LoadingPage UI 구성

--- src/app/loading/page.tsx
+++ src/app/loading/page.tsx
@@ -1,13 +1,22 @@
-export default function Home() {
-    return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <p>triptotravel</p>
+"use client";
+import Header from "@/components/common/Header";
+
+export default function LoadingPage() {
+  return (
+    <div className="min-h-screen flex flex-col justify-between bg-white">
+      <Header variation="combine" />
+
+      <main className="h-full flex flex-col items-center justify-center m-auto">
+        
+        {/* 로딩 애니메이션 배경화면 추가해야 함*/}
+
+        <div className="flex flex-col items-center justify-center gap-5">
+          <div className="cursor-pointer w-[120px] h-[120px] rounded-[14px] bg-zinc-300 flex items-center justify-center shadow-md text-[32px]">
+            로딩 아이콘
           </div>
-        </main>
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        </footer>
-      </div>
-    );
-  }
+          <p className="text-[18px] font-semibold text-center w-full">여행기를 생성 중입니다.</p>
+        </div>
+      </main>
+    </div>
+  );
+}
