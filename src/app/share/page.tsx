48527cb12852081468180f7ec490a3a840b1d50a
NEWJIN <winniepooh00@naver.com>
2025-04-26 08:41:20 +0900

    feat: SharePage UI 구성

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -1,13 +1,34 @@
-export default function Home() {
-    return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <p>triptotravel</p>
+"use client";
+
+import { useRouter } from "next/navigation";
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
+import LogThumbnail from "@/components/common/LogThumbnail";
+import CTAButton from "@/components/buttons/CTAButton";
+
+export default function SharePage() {
+  const router = useRouter();
+
+  const handleNext = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col justify-between bg-white">
+      <Header variation="type-back" />
+
+      <main className="flex flex-col items-center justify-center my-[60px] gap-[60px]">
+        <section className="w-full flex flex-col gap-[30px] items-center">
+          <LogThumbnail imageUrl="/images/testimage.jpg" />
+          <div className="flex flex-col gap-4">
+            <CTAButton variation="default" label="저장하기" />
+            <CTAButton variation="default" label="공유하기" />
           </div>
-        </main>
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        </footer>
-      </div>
-    );
-  }
+        </section>
+        <CTAButton variation="black" label="처음부터 다시하기" onClick={handleNext} />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
