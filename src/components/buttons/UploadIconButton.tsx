df9d60d6973b0d4ecadc85771901037bba6caf57
NEWJIN <winniepooh00@naver.com>
2025-05-18 18:37:45 +0900

    feat: upload icon 추가

--- src/components/buttons/UploadIconButton.tsx
+++ src/components/buttons/UploadIconButton.tsx
@@ -1,15 +1,15 @@
-import { ChangeEvent } from "react"
+import { ChangeEvent } from "react";
 
 type UploadIconButtonProps = {
-  onUpload: (files: FileList) => void
-}
+  onUpload: (files: FileList) => void;
+};
 
 export default function UploadIconButton({ onUpload }: UploadIconButtonProps) {
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
-      onUpload(e.target.files)
+      onUpload(e.target.files);
     }
-  }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center gap-5">
@@ -24,11 +24,19 @@ export default function UploadIconButton({ onUpload }: UploadIconButtonProps) {
 
       <label
         htmlFor="upload"
-        className="cursor-pointer w-[120px] h-[120px] rounded-[14px] bg-zinc-300 flex items-center justify-center shadow-md text-[32px]"
+        className="cursor-pointer w-[120px] h-[120px] rounded-[14px] flex items-center justify-center"
       >
-        업로드 아이콘
+        <img
+          src="/images/imageicon.svg"
+          alt="업로드 아이콘"
+          className="w-40 h-40 object-cover"
+        />
       </label>
-      <p className="text-[18px] font-semibold text-center w-full">아이콘을 클릭해<br/>사진을 업로드하세요</p>
+      <p className="text-[18px] font-semibold text-center w-full">
+        아이콘을 클릭해
+        <br />
+        사진을 업로드하세요
+      </p>
     </div>
-  )
+  );
 }
