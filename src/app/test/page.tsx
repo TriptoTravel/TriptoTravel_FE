cfff8beb3921b225163ff4d79105388eae7bf347
NEWJIN <winniepooh00@naver.com>
2025-05-19 23:24:42 +0900

    remove: test 페이지 제거

--- src/app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestUploadPage() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-[url('/images/background2.svg')] bg-cover bg-center">
-      <img src="/images/text.svg" alt="" />
-    </div>
-  );
-}
